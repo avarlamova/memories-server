f20e407e402b71d71665e3be8ed38466bc733283
avarlamova <59831804+avarlamova@users.noreply.github.com>
2022-05-20 20:50:33 +0300

    initial backend configuration

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import express from "express";
 import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
+app.use("/posts", postRoutes); // по адресу localhost:3000/posts будут postRoutes
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-/// ggg
+const CONNECTION_URL =
+  "mongodb+srv://admin:12345@cluster0.hwrz9.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("server running on port " + PORT);
+    })
+  )
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// mongoose.set("useFindAndModify", false);
